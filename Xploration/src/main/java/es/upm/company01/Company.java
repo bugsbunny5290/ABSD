777755d8df9a6735f75c90a5f0af85e99a28bd9b
sarahjessi <sarahjessi@gmail.com>
2016-05-06 10:31:11 -0500

    Change the Company and Spacecraft

--- Xploration/src/main/java/es/upm/company01/Company.java
+++ Xploration/src/main/java/es/upm/company01/Company.java
@@ -17,78 +17,116 @@ import jade.content.onto.basic.*;
 
 import java.util.Date;
 
+import es.upm.ontology.RegistrationRequest;
+import es.upm.ontology.XplorationOntology;
+
 public class Company extends Agent {
+
+	// Codec for the SL language used and instance of the ontology
+	private Codec codec = new SLCodec();
+
+	// Declare Ontology
+	public XplorationOntology ontology = (XplorationOntology) XplorationOntology.getInstance();
 	
-	public final static String REGISTRATION = "Registration";
+	// Name of Spacecraft
 	public final static String SPACECRAFT = "SPACECRAFT";
-	public final static String COMPANY_X = "CompanyX";
-	
-	protected void setup()
-	{
-		System.out.println(getLocalName()+": has entered");
-		
-		addBehaviour(new SimpleBehaviour(this)
-		{
+
+
+	protected void setup() {
+		System.out.println(getLocalName() + ": has entered");
+
+		// Register of the codec and the ontology to be used in the ContentManager
+		getContentManager().registerLanguage(codec);
+		getContentManager().registerOntology(ontology);
+
+		addBehaviour(new SimpleBehaviour(this) {
 			AID ag;
 			boolean end = false;
-			
-			public void action()
-			{
+
+			public void action() {
 				// Creates the description for the type of agent to be searched
 				DFAgentDescription dfd = new DFAgentDescription();
 				ServiceDescription sd = new ServiceDescription();
 				sd.setType(SPACECRAFT);
 				dfd.addServices(sd);
-				
+
 				try {
-				
+
 					// It finds agents of the required type
 					DFAgentDescription[] res = new DFAgentDescription[20];
 					res = DFService.search(myAgent, dfd);
-					
+
 					// Gets the first occurrence, if there was success
-					if (res.length > 0)
-					{
-						System.out.println(getLocalName()+": found Spacecraft");
-						
-						ag = (AID)res[0].getName();
-						
+					if (res.length > 0) {
+						System.out.println(getLocalName() + ": found Spacecraft");
+
+						ag = (AID) res[0].getName();
+
 						// Asks request to the spacecraft
 						ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
 						msg.setSender(getAID());
+
+						msg.setLanguage(codec.getName());
+						msg.setOntology(ontology.getName());
+
+						msg.setProtocol(ontology.PROTOCOL_REGISTRATION);
+
 						msg.addReceiver(ag);
-						msg.setProtocol(REGISTRATION);
-						
-						msg.setContent(COMPANY_X);
-						
+
+						// Register the company
+						RegistrationRequest obj = new RegistrationRequest();
+
+						// Name of company
+						obj.setCompany("COMPANY01");
+
+						// Package the Content
+						getContentManager().fillContent(msg, new Action(getAID(), obj));
+
 						send(msg);
-						System.out.println(getLocalName()+": asks for Request");
+
+						System.out.println(getLocalName() + ": asks for Request");
+
+						// Waiting the answer
+						// ACLMessage msg2 = blockingReceive();
+
+						ACLMessage msg2 = blockingReceive(
+							MessageTemplate.and(
+								MessageTemplate.and(MessageTemplate.MatchLanguage(codec.getName()),
+										MessageTemplate.MatchOntology(ontology.getName())),
+								MessageTemplate.MatchProtocol(ontology.PROTOCOL_REGISTRATION)));
+
+						if (msg2 != null) {
+							System.out.println(getLocalName() + ": Received the following message: ");
+							System.out.println(msg2.toString());
+							end = true;
+						}
 						
-						//Waiting the answer
-			            ACLMessage msg2 = blockingReceive();
-			            if (msg2!= null)
-			            {
-			                System.out.println(getLocalName() + ": Received the following message: ");
-			                System.out.println(msg2.toString());
-			                end = true;
-			            }
-						
-						//end = true;
-						
+						msg2 = blockingReceive(
+								MessageTemplate.and(
+									MessageTemplate.and(MessageTemplate.MatchLanguage(codec.getName()),
+											MessageTemplate.MatchOntology(ontology.getName())),
+									MessageTemplate.MatchProtocol(ontology.PROTOCOL_REGISTRATION)));
+
+							if (msg2 != null) {
+								System.out.println(getLocalName() + ": Received the following message: ");
+								System.out.println(msg2.toString());
+								end = true;
+							}
+
+						// end = true;
+
 						doWait(5000);
 					}
-				}catch (Exception e) 
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-			
-			public boolean done ()
-			{
+
+			public boolean done() {
 				return end;
 			}
 		});
-		
+
 	}
-	
+
 }
--- Xploration/src/main/java/es/upm/platform01/Spacecraft.java
+++ Xploration/src/main/java/es/upm/platform01/Spacecraft.java
@@ -19,141 +19,166 @@ import jade.content.onto.basic.*;
 import java.util.Calendar;
 import java.util.Date;
 
+import es.upm.ontology.RegistrationRequest;
+import es.upm.ontology.XplorationOntology;
+
 public class Spacecraft extends Agent {
-	
-	private static final long serialVersionUID =1L;
-	public final static String REGISTRATION = "Registration";
+
+	// Codec for the SL language used and instance of the ontology
+	private Codec codec = new SLCodec();
+
+	// Declare Ontology
+	public XplorationOntology ontology = (XplorationOntology) XplorationOntology.getInstance();
+
+	private static final long serialVersionUID = 1L;
 	public final static String SPACECRAFT = "Spacecraft";
-	public final static String REQUEST = "REQUEST";
 	public final static String COMPANY_X = "Company6";
-	public final static String AGREE = "AGREE";
-	public final static String NOT_UNDERSTOOD = "NOT_UNDERSTOOD";
-	public final static String REFUSE = "REFUSE";
-	public final static String FAILURE = "FAILURE";
-	public final static String REGISTER = "REGISTER";
-	//AID[] registeredCompanies = new AID[20];
-	
-	protected void setup()
-	{
+
+	// AID[] registeredCompanies = new AID[20];
+
+	protected void setup() {
 		final Calendar registerTime_Begin = Calendar.getInstance();
 		final Calendar registerTime_End = Calendar.getInstance();
 		final Calendar Currentime = Calendar.getInstance();
-		
+
+		// Register of the codec and the ontology to be used in the
+		// ContentManager
+		getContentManager().registerLanguage(codec);
+		getContentManager().registerOntology(ontology);
+
 		registerTime_Begin.set(2016, 3, 19);
-		registerTime_End.set(2016, 3, 27);
+		registerTime_End.set(2016, 12, 27);
 		Currentime.getTime();
-		
-		System.out.println(getLocalName()+": has entered");
-		System.out.println(getLocalName()+": Period of Registration: "+ registerTime_Begin.getTime() + " - " + registerTime_End.getTime());
-		System.out.println(getLocalName()+": Current: "+ Currentime.getTime());
-		
-		try
-		{
+
+		System.out.println(getLocalName() + ": has entered");
+		System.out.println(getLocalName() + ": Period of Registration: " + registerTime_Begin.getTime() + " - "
+				+ registerTime_End.getTime());
+		System.out.println(getLocalName() + ": Current: " + Currentime.getTime());
+
+		try {
 			// Creates its own description
 			DFAgentDescription dfd = new DFAgentDescription();
 			ServiceDescription sd = new ServiceDescription();
 			sd.setName(this.getName());
 			sd.setType(SPACECRAFT);
 			dfd.addServices(sd);
-			
+
 			// Registers its description in the DF
 			DFService.register(this, dfd);
-			System.out.println(getLocalName()+": registered in the DF");
+			System.out.println(getLocalName() + ": registered in the DF");
 			dfd = null;
 			sd = null;
 			doWait(10000);
-		} 
-		catch (FIPAException e)
-		{
+		} catch (FIPAException e) {
 			e.printStackTrace();
 		}
-		
-		addBehaviour(new SimpleBehaviour(this)
-		{
+
+		addBehaviour(new SimpleBehaviour(this) {
 			boolean end = false;
-			private static final long serialVersionUID =1L;
-			
-			public void action()
-			{
+			private static final long serialVersionUID = 1L;
+
+			public void action() {
 				// Waits for answer to the request
-				ACLMessage msg = receive();
-				
+				ACLMessage msg = receive(MessageTemplate.and(
+						MessageTemplate.and(MessageTemplate.MatchLanguage(codec.getName()),
+								MessageTemplate.MatchOntology(ontology.getName())),
+						MessageTemplate.and(MessageTemplate.MatchProtocol(ontology.PROTOCOL_REGISTRATION),
+								MessageTemplate.MatchPerformative(ACLMessage.REQUEST))));
 				try {
-				
-					if (msg != null)
-					{
-						//Between the Period of Registration (Boundary included)
-						if ( inThePeriod(registerTime_Begin, registerTime_End, Currentime) )
-						{
-							ACLMessage reply = msg.createReply();
-							reply.setContent(REFUSE);
-							myAgent.send(reply);
-							System.out.println(myAgent.getLocalName()+": answer sent -> "+reply.getContent());
-							end = true;
-						} else { 
-						
-							if ((msg.getContent()).contains(COMPANY_X))
-							{
-								// If an Request arrives, it answers with the ....
-								System.out.println(myAgent.getLocalName()+": received a request from "+(msg.getSender()).getLocalName()); 
-								ACLMessage reply = msg.createReply();		
-								reply.setContent(AGREE);
-								myAgent.send(reply);
-								System.out.println(myAgent.getLocalName()+": answer sent -> "+reply.getContent());
-								
-								doWait(5000);
-								
-								// Send the answer
-			                    System.out.println(getLocalName() +": Send answer");
-			                    ACLMessage respond = new ACLMessage( ACLMessage.INFORM );
-			                    respond.setContent( REGISTER );
-			                    respond.addReceiver( msg.getSender() );
-			                    send(respond);
-			                    System.out.println(getLocalName() +": Sent Register to Company");
-			                    System.out.println(respond.toString());
-			                    end = true;
-								
-							} else {
-								ACLMessage reply = msg.createReply();
-								reply.setContent(NOT_UNDERSTOOD);
-								myAgent.send(reply);
-								
-								System.out.println(myAgent.getLocalName()+": answer sent -> "+reply.getContent());
-								end = true;
+					ContentElement ce = null;
+					if (msg != null) {
+						// The ContentManager transforms the message content
+						// (string)
+						// in java objects
+						// Unpacking the content
+						ce = getContentManager().extractContent(msg);
+						// We expect an action inside the message
+						if (ce instanceof Action) {
+							Action agAction = (Action) ce;
+							Concept conc = agAction.getAction();
+							// If the action is EstimationRequest...
+							if (conc instanceof RegistrationRequest) {
+								RegistrationRequest request = (RegistrationRequest) conc;
+								request.getCompany();
+								// Between the Period of Registration (Boundary
+								// included)
+								if (inThePeriod(registerTime_Begin, registerTime_End, Currentime)) {
+									ACLMessage reply = msg.createReply();
+									//reply.setContent(REFUSE);
+									myAgent.send(reply);
+									System.out
+											.println(myAgent.getLocalName() + ": answer sent -> " + reply.getContent());
+									end = true;
+								} else {
+
+									//if ((msg.getContent()).contains(COMPANY_X)) {
+										// If an Request arrives, it answers
+										// with the ....
+										System.out.println(myAgent.getLocalName() + ": received a request from "
+												+ (msg.getSender()).getLocalName());
+										ACLMessage reply = msg.createReply();
+										reply.setPerformative(ACLMessage.AGREE);
+										myAgent.send(reply);
+										System.out.println(
+												myAgent.getLocalName() + ": answer sent -> " + reply.getContent());
+
+										doWait(5000);
+
+										// Send the answer
+										System.out.println(getLocalName() + ": Send answer");
+										ACLMessage respond = msg.createReply();
+										respond.setPerformative(ACLMessage.INFORM);
+										respond.addReceiver(msg.getSender());
+										send(respond);
+										System.out.println(getLocalName() + ": Sent Register to Company");
+										System.out.println(respond.toString());
+										end = true;
+
+									/*} else {
+										ACLMessage reply = msg.createReply();
+										reply.setContent(NOT_UNDERSTOOD);
+										myAgent.send(reply);
+
+										System.out.println(
+												myAgent.getLocalName() + ": answer sent -> " + reply.getContent());
+										end = true;
+									}*/
+								}
 							}
+
 						}
-					} 
-				} catch (Exception e) 
-				{
+					}
+				} catch (Exception e) {
 					ACLMessage reply = msg.createReply();
-					reply.setContent(FAILURE);
+					//reply.setContent(FAILURE);
 					myAgent.send(reply);
-					
-					System.out.println(myAgent.getLocalName()+": answer sent -> "+reply.getContent());
+
+					System.out.println(myAgent.getLocalName() + ": answer sent -> " + reply.getContent());
 					end = true;
-					
+
 					e.printStackTrace();
-					
+
 				}
 			}
-			public boolean done()
-            {
-                return end;
-            }
+
+			public boolean done() {
+				return end;
+			}
 		});
 	}
-	
-	public boolean inThePeriod ( Calendar registerTime_Begin, Calendar registerTime_End, Calendar Currentime ) {
-		
+
+	public boolean inThePeriod(Calendar registerTime_Begin, Calendar registerTime_End, Calendar Currentime) {
+
 		if (Currentime.getTime().equals(registerTime_Begin.getTime()))
 			return false;
 		if (Currentime.getTime().equals(registerTime_End.getTime()))
 			return false;
-		if (Currentime.getTime().before(registerTime_Begin.getTime()) || Currentime.getTime().after(registerTime_End.getTime()))
-		{
+		if (Currentime.getTime().before(registerTime_Begin.getTime())
+				|| Currentime.getTime().after(registerTime_End.getTime())) {
 			return true;
-		} else return false;
-		
+		} else
+			return false;
+
 	}
 
 }
